073344a1c3caf4b6e367f5db13556fd1101de322
Alexis Clément <clemalex20@gmail.com>
2019-07-11 00:57:00 +0200

    testing on volume oscilator

--- PyRandSound/effects/volume_oscillator.py
+++ PyRandSound/effects/volume_oscillator.py
@@ -1,6 +1,8 @@
 from pygame import mixer
 from ._base import EffectsBase
 from ..modificators import *
+import random
+import time
 
 
 class VolumeOscillator(EffectsBase):
@@ -11,17 +13,17 @@ class VolumeOscillator(EffectsBase):
         self.step = step
 
     def play(self, modificator='relu'):
-    	x = self.channel.get_volume()
-    	sound = mixer.Sound(self.path)
-    	channel.play(sound)
+        x = self.channel.get_volume()
+        sound = mixer.Sound(self.path)
+        self.channel.play(sound)
 
-    	while(channel.get_busy()):
-    		rand = random.random()
-    		if rand < eval(modificator)(x):
-    			x = x - self.step
-    		else:
-    			x = x + self.step
-    		time.sleep(self.step)
-    		channel.set_volume(x)
+        while(self.channel.get_busy()):
+            rand = random.random()
+            if rand < eval(modificator)(x):
+                x = x - self.step
+            else:
+                x = x + self.step
+            time.sleep(self.step)
+            self.channel.set_volume(x)
         if self.continue_loop():
             self.play()
--- test/test.py
+++ test/test.py
@@ -1,11 +1,14 @@
 from PyRandSound.player import Player
 from PyRandSound.effects.random_timing import RandomTiming
+from PyRandSound.effects.volume_oscillator import VolumeOscillator
 
 def test_2_random_timing():
-    effect1 = RandomTiming('sounds/sound5.wav', 2, [2, 5])
-    effect2 = RandomTiming('sounds/sound6.wav', 5, [5, 10])
+    # effect1 = RandomTiming('sounds/sound5.wav', 2, [2, 5])
+    # effect2 = RandomTiming('sounds/sound6.wav', 5, [5, 10])
+    effect3 = VolumeOscillator('sounds/sound4.wav', True)
 
     player = Player()
-    player.add_effect(effect1)
-    player.add_effect(effect2)
+    # player.add_effect(effect1)
+    # player.add_effect(effect2)
+    player.add_effect(effect3)
     player.play()
